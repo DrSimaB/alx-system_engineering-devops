8fa0a999ca096b630025cf8c6bb572e5226bd909
DrSimaB <DrSimaB>
2023-03-24 10:18:36 -0700

    We move

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#config ssh using puppet
+include stdlib
+file_line { 'private keyphers':
+ensure => present,
+path   => '/etc/ssh/ssh_config',
+line   => 'IdentityFile ~/.ssh/school',
+}
+file_line { 'refuse authenticate using a password':
+ensure => present,
+path   => '/etc/ssh/ssh_config',
+line   => 'PasswordAuthentication no',
+}
